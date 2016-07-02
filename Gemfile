83ab0b6a6ac9bb54b9bb9e6d7b5b0b5748678274
Perry Kibler <pk@unleashed.io>
2016-07-01 19:48:26 -0600

    mm-content is working again

--- Gemfile
+++ Gemfile
@@ -2,8 +2,6 @@ source 'https://rubygems.org'
 
 gem 'middleman'
 gem 'middleman-livereload'
-
 gem 'slim'
-
 gem 'middleman-dotenv'
-gem 'contentful_middleman', :git => 'https://github.com/contentful/contentful_middleman.git', :ref => '2e8a831a807e0ae4fc19bf2178e8d183e41321e0'
+gem 'contentful_middleman', :git => 'https://github.com/contentful/contentful_middleman.git', :ref => 'ba9628902afde36d7e876f99ffc6c3b79598940a'
--- config.rb
+++ config.rb
@@ -18,25 +18,26 @@ end
 # pretty URLs
 activate :directory_indexes
 
+# hide private keys
 activate :dotenv
 
 # contentful info
 activate :contentful do |f|
-  f.access_token  = ENV['CONTENTFUL_TOKEN']
-  f.space         = { playground: 'sihh9h4tsjaz' }
-  f.cda_query       = { limit: 10000 }
+  f.space = { playground: 'sihh9h4tsjaz' }
+  f.access_token = ENV['CONTENTFUL_TOKEN']
+  f.cda_query = { limit: 10000 }
   f.content_types = {
     blogPost: 'k5KOOOcoDKgAsUAOgaKMg'
   }
 end
 
-# page "/post/#{blogPost[1][:slug]}.html", layout: 'layout'
-# locals: { blogPost: OpenStruct.new(blogPost[1]) }
 if Dir.exist?(config.data_dir)
   data.playground.blogPost.each do |id, blogPost|
     proxy "post/#{ blogPost.slug }", "post/template.html", :ignore => true, layout: 'layout'
   end
 end
+# page "/post/#{blogPost[1][:slug]}.html"
+# locals: { blogPost: OpenStruct.new(blogPost[1]) }
 
 # using markdown for contentful stuff
 set :markdown_engine, :kramdown
